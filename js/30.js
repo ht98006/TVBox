ea51f57682d27457468cb801536ac301d71751ab
ht98006 <57003780+ht98006@users.noreply.github.com>
2023-07-06 07:14:47 +0800

    Add files via upload

---
+++ js/30.js
@@ -0,0 +1,5 @@
+var rule = Object.assign(muban.mxpro,{
+            title: '櫻花動漫',
+            host: 'http://www.dm99.me',  
+	    class_parse: '.row&&ul&&li&&li;a&&Text;a&&href;.*/(.*?).html',
+});
