fc08f6fe1442b5bdba7c788ddddcdcd948f2248c
ht98006 <57003780+ht98006@users.noreply.github.com>
2023-07-12 13:18:29 +0800

    Delete 30.js

--- js/30.js
+++
@@ -1,5 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-            title: '櫻花動漫',
-            host: 'http://www.dm99.me',  
-	    class_parse: '.row&&ul&&li&&li;a&&Text;a&&href;.*/(.*?).html',
-});
